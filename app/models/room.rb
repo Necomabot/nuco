de04f6c336bc99d84b564bad7d53561661673fdd
NECOMAbot <n88.inokuchi.yuma@gmail.com>
2021-02-10 17:21:28 +0900

    Update room.rb

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,9 @@
 class Room < ApplicationRecord
 
+  validates :category_id, numericality: { other_than: 1 } 
+
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
 end
